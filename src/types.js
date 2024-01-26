fd22780a9bd9f582dfd5f0b9f11b17e8db765583
Patrick White <patrickwhite@Patricks-MacBook-Pro.local>
2024-01-26 08:45:11 -0600

    build: install firebase

---
+++ src/types.js
@@ -0,0 +1,12 @@
+// type CollectionItem {
+//     id: string;
+//     caption: String;
+//     likes: string[];
+//     comments: CommentDocument[];
+//     createdAt: Date;
+//     createdBy: String;
+//     details: {
+//         // card details
+//     }
+// }
+
