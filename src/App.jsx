cf8b66f599456a4decbc7f4990b30e04186e2e8a
Patrick White <patrickwhite@Patricks-MacBook-Pro.local>
2024-01-29 14:39:42 -0600

    feat: set up chakra dark-theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@chakra-ui/react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    Hi
+    <Button>
+      Hello World
+    </Button>
   </>
   )
 }
