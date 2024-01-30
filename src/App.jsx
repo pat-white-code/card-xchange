aba0639f989e195c30f0bca1050a95b963a30fe5
Patrick White <patrickwhite@Patricks-MacBook-Pro.local>
2024-01-30 09:44:04 -0600

    feat: add jolly roger images to public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from '@chakra-ui/react'
 import { Route, Routes } from 'react-router-dom'
 import AuthPage from './pages/AuthPage/AuthPage.jsx'
 import HomePage from './pages/HomePage/Homepage.jsx'
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,11 +1,18 @@
-import { Container, Flex } from '@chakra-ui/react'
+import { Box, Container, Flex, Image, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 const AuthPage = () => {
     return (
-        <Flex border={'1px red solid'}minHeight={'100vh'} minWidth={''} justifyContent={'center'} alignItems={'center'} px={4}>
-            <Container padding={0}>
-                auth
+        <Flex minHeight={'100vh'} minWidth={''} justifyContent={'center'} alignItems={'center'} px={4}>
+            <Container minW={'container.md'} px={0} >
+                {/* Left Side */}
+                <Box display={{base: 'none', md: 'block'}}>
+                    <Image src="public/Jolly Roger mag 261413bc-0a0c-445a-9749-2d5c64e89a7a.png" alt="phone-pic" borderRadius={'10px'}/>
+                {/* Right Side */}
+                </Box>
+                <div>
+                    Authenticate here
+                </div>
             </Container>
         </Flex>
     )
